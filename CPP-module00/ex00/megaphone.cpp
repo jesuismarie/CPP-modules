44c243214e189043fc5645cd267f1b6326c27b8a
Marie Nazaryan <mari.nazaryan7173@gmail.com>
2024-06-15 17:47:56 +0400

    module00: ex00

---
+++ CPP-module00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int	i, j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	i = 0;
+	while (argv[++i])
+	{
+		j = -1;
+		while (argv[i][++j])
+			std::cout << (char)std::toupper(argv[i][j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
