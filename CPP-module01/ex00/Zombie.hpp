206afbe5acdb02e92b677ebf8ce936b12010bb8d
kali-linux <mari.nazaryan7173@gmail.com>
2024-06-22 00:11:20 +0400

    cpp01: ex00

---
+++ CPP-module01/ex00/Zombie.hpp
@@ -0,0 +1,18 @@
+#ifndef _ZOMBIE_HPP_
+# define _ZOMBIE_HPP_
+
+# include <iostream>
+
+class Zombie
+{
+private:
+	std::string	name;
+public:
+	Zombie(std::string name);
+	~Zombie();
+	void		announce(void);
+	std::string	get_name(void) const;
+	void		set_name(std::string str);
+};
+
+#endif
---
+++ CPP-module01/ex00/main.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+Zombie	*newZombie(std::string name);
+void	randomChump(std::string name);
+
+int	main(void)
+{
+	Zombie	*z1 = newZombie("Pxik");
+	z1->announce();
+	delete z1;
+	randomChump("Ando");
+	{
+		Zombie z2("Avocado");
+		z2.announce();
+	}
+	return (0);
+}
