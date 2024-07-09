bb248479068d7f6ccac8c6ead05c3672643873ce
Marie Nazaryan <mari.nazaryan7173@gmail.com>
2024-07-09 13:36:25 +0400

    cpp02: ex02

---
+++ CPP-module02/ex02/Fixed.hpp
@@ -0,0 +1,50 @@
+#ifndef _FIXED_HPP_
+# define _FIXED_HPP_
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+private:
+	int					_num;
+	static const int	_fract;
+public:
+	Fixed(void);
+	Fixed(const int &value);
+	Fixed(const float &value);
+	Fixed(Fixed const &copy);
+	~Fixed();
+	
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+	/*---------------------- Arithmetic operators ----------------------*/
+	Fixed	&operator=(Fixed const &copy);
+	Fixed	operator*(Fixed const &other_num) const;
+	Fixed	operator/(Fixed const &other_num) const;
+	Fixed	operator+(Fixed const &other_num) const;
+	Fixed	operator-(Fixed const &other_num) const;
+	/*---------------------- Comparison operators ----------------------*/
+	bool	operator>(Fixed const &other_num) const;
+	bool	operator<(Fixed const &other_num) const;
+	bool	operator>=(Fixed const &other_num) const;
+	bool	operator<=(Fixed const &other_num) const;
+	bool	operator!=(Fixed const &other_num) const;
+	bool	operator==(Fixed const &other_num) const;
+	/*------------------ Increment/Decrement operators -----------------*/
+	Fixed	&operator++();
+	Fixed	operator++(int);
+	Fixed	&operator--();
+	Fixed	operator--(int);
+	/*--------------------- Static member functions --------------------*/
+	static Fixed		min(Fixed &a, Fixed &b);
+	static const Fixed	min(Fixed const &a, Fixed const &b);
+	static Fixed		max(Fixed &a, Fixed &b);
+	static const Fixed	max(Fixed const &a, Fixed const &b);
+};
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif
