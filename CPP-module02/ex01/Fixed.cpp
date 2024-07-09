b89e8ffe6dd15059eee1bb060cee2862c10c5a6f
Marie Nazaryan <mari.nazaryan7173@gmail.com>
2024-07-09 13:36:13 +0400

    cpp02: ex01

---
+++ CPP-module02/ex01/Fixed.cpp
@@ -0,0 +1,62 @@
+#include "Fixed.hpp"
+
+const int	Fixed::_fract = 8;
+
+Fixed::Fixed(void): _num(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int &value): _num(value << _fract)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float &value): _num(roundf(value * (1 << _fract)))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(Fixed const &copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &copy)
+		this->_num = copy.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (this->_num);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_num = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)this->_num / (1 << _fract));
+}
+
+int	Fixed::toInt(void) const
+{
+	return (this->_num >> this->_fract);
+}
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &fixed)
+{
+	return (os << fixed.toFloat());
+}
---
+++ CPP-module02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef _FIXED_HPP_
+# define _FIXED_HPP_
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+private:
+	int					_num;
+	static const int	_fract;
+public:
+	Fixed(void);
+	Fixed(const int &value);
+	Fixed(const float &value);
+	Fixed(Fixed const &copy);
+	~Fixed();
+	Fixed	&operator=(Fixed const &copy);
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+};
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif
