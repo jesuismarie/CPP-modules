3e5ada942c51cfe516b16604f4cd8d76ece3dbaa
Marie <mari.nazaryan7173@gmail.com>
2024-08-07 14:55:55 +0400

    cleanup

--- CPP-module01/ex02/main.cpp
+++ CPP-module01/ex02/main.cpp
@@ -2,9 +2,9 @@
 
 int	main(void)
 {
-	std::string str = "HI THIS IS BRAIN";
-	std::string *stringPTR = &str;
-	std::string &stringREF = str;
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
 
 	std::cout << "The memory address of the str " << &str << std::endl;
 	std::cout << "The memory address of the stringPTR: " << stringPTR << std::endl;
