31eeac86f441acf9f6e3e051c71ee758fde4c3c3
Marie <mari.nazaryan7173@gmail.com>
2024-10-29 11:29:55 +0400

    cleanup

--- CPP-module07/ex00/main.cpp
+++ CPP-module07/ex00/main.cpp
@@ -4,25 +4,26 @@ class Awesome
 {
 	public:
 		Awesome(void) : _n(0) {}
-		Awesome( int n ) : _n( n ) {}
-		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
-		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
-		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
-		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
-		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
-		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
-		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
-		int get_n() const { return _n; }
+		Awesome(int n) : _n(n) {}
+		Awesome&	operator= (Awesome & a) { _n = a._n; return *this;}
+		bool		operator==(const Awesome& rhs) const {return (this->_n == rhs._n);}
+		bool		operator!=(const Awesome& rhs) const{return (this->_n != rhs._n);}
+		bool		operator>(const Awesome& rhs) const {return (this->_n > rhs._n);}
+		bool		operator<(const Awesome& rhs) const {return (this->_n < rhs._n);}
+		bool		operator>=(const Awesome& rhs) const {return (this->_n >= rhs._n);}
+		bool		operator<=(const Awesome& rhs) const {return (this->_n <= rhs._n);}
+		int			get_n() const {return _n;}
 	private:
-		int _n;
+		int	_n;
 };
-std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+std::ostream&	operator<<(std::ostream & o, const Awesome &a) {o << a.get_n(); return o;}
 
 
 int	main(void)
 {
-	int a = 2;
-	int b = 3;
+	int	a = 2;
+	int	b = 3;
 	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
@@ -34,8 +35,7 @@ int	main(void)
 	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
 	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 
-	Awesome a1(2), b1(4);
-
+	Awesome	a1(2), b1(4);
 	swap(a1, b1);
 	std::cout << a1 << " " << b1 << std::endl;
 	std::cout << max(a1, b1) << std::endl;
